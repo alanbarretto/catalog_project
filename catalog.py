8d33e04440e2a37ac84835b9228824ffc694e13f
Alan Barretto <alanbarretto@gmail.com>
2017-11-27 22:09:35 -0800

    add arguments to functions that require arguments

--- catalog.py
+++ catalog.py
@@ -7,11 +7,11 @@ def catalogHome():
 	return "This is the home page"
 
 @app.route('/catalog/<int:catalog_id>/items')
-def catalogItems():
+def catalogItems(catalog_id):
 	return "This is the page where the specific catalog displays with all the items under it"
 
 @app.route('/catalog/<int:catalog_id>/items/<int:item_id>')
-def specificItem():
+def specificItem(catalog_id, item_id):
 	return "This is the page of a specific item under a category"
 
 @app.route('/catalog/create')
@@ -19,23 +19,23 @@ def createCatalog():
 	return "This is the page where you are able to create a new catalog category"
 
 @app.route('/catalog/<int:catalog_id>/edit')
-def editCatalog():
+def editCatalog(catalog_id):
 	return "This is where you can edit a category"
 
 @app.route('/catalog/<int:catalog_id>/delete')
-def deleteCatalog():
+def deleteCatalog(catalog_id):
 	return "This is where you delete a category"
 
 @app.route('/catalog/<int:catalog_id>/items/create')
-def createItem():
+def createItem(catalog_id):
 	return "This page is for creating a new item"
 
 @app.route('/catalog/<int:catalog_id>/items/<int:item_id>/edit')
-def editItem():
+def editItem(catalog_id, item_id):
 	return "This is where you can edit an item"
 
 @app.route('/catalog/<int:catalog_id>/items/<int:item_id>/delete')
-def deleteItem():
+def deleteItem(catalog_id, item_id):
 	return "You can delete an item here"
 
 
