8a78ccc1b9525a2254ece0c1e4c1568af9348ce2
Alan Barretto <alanbarretto@gmail.com>
2017-12-31 16:52:29 -0800

    Add the user helper functions

--- catalog.py
+++ catalog.py
@@ -24,6 +24,30 @@ session = DBSession()
 from flask import session as login_session
 import random, string
 
+#User Helper Functions
+
+def createUser(login_session):
+	newUser = User(name=login_session['username'], email=login_session['email'], picture=login_session['picture'])
+	session.add(newUser)
+	session.commit()
+	user = session.query(User).filter_by(email=login_session['email']).one()
+	return user.id
+
+def getUserInfo(user_id):
+	user= session.query(User).filter_by(id=user_id).one()
+	return userinfo_url
+
+def getUserID(email):
+	try:
+		user = session.query(User).filter_by(email=email).one()
+		return user.id
+	except:
+		return None
+
+
+
+
+
 #Jasonify functions
 
 @app.route("/category/JSON")
